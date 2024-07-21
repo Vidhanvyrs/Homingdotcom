42e079949dcd4dc95af633bf913071824f381b06
Vidhanvyrs <vidhanvyrs@gmail.com>
2024-07-22 00:34:24 +0530

    added defer from router dom for loading indicator

--- client/src/lib/loaders.js
+++ client/src/lib/loaders.js
@@ -1,3 +1,4 @@
+import { defer } from "react-router-dom";
 import apiRequest from "./apiRequest.js";
 //going to use react router dom inordr to use fetching of the data just like we used react query
 export const singlePageLoader = async ({ request, params }) => {
@@ -9,8 +10,11 @@ export const listPageLoader = async ({ request, params }) => {
   // console.log(request)
   //getting the url using ?
   const query = request.url.split("?")[1];
-  const res = await apiRequest("/posts?" + query);
-  return res.data;
+  const postPromise = apiRequest("/posts?" + query); //goin to remove await for using defer from react router dom
+  //   return res.data;
+  return defer({
+    postResponse: postPromise, //our post response which we are sending to show it in frontend
+  });
   // const res = await apiRequest("/posts/" + params.id);
   // return res.data;
 };
--- client/src/routes/listPage/listPage.jsx
+++ client/src/routes/listPage/listPage.jsx
@@ -3,24 +3,45 @@ import "./listPage.scss";
 import Filter from "../../components/filter/Filter";
 import Card from "../../components/card/Card";
 import Map from "../../components/map/Map";
-import { useLoaderData } from "react-router-dom";
+import { Await, useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
 
 function ListPage() {
   // const data = listData;
-  const posts = useLoaderData();//fetching the post from db using loader
-
+  const data = useLoaderData(); //fetching the post from db using loader
+  //using defer from react router dom for adding loadin...
   return (
     <div className="listPage">
       <div className="listContainer">
         <div className="wrapper">
           <Filter />
-          {posts.map((item) => (
+          {/* {posts.map((item) => (
             <Card key={item.id} item={item} />
-          ))}
+          ))} */}
+          <Suspense fallback={<p>Loading..:|</p>}>
+            <Await
+              resolve={data.postResponse}
+              errorElement={<p>Error loading posts!</p>}
+            >
+              {(postResponse) =>
+                postResponse.data.map((post) => (
+                  <Card key={post.id} item={post} />
+                ))
+              }
+            </Await>
+          </Suspense>
         </div>
       </div>
       <div className="mapContainer">
-        <Map items={posts} />
+        {/* <Map items={posts} /> */}
+        <Suspense fallback={<p>Loading..:|</p>}>
+          <Await
+            resolve={data.postResponse}
+            errorElement={<p>Error loading posts!</p>}
+          >
+            {(postResponse) => <Map items={postResponse.data} />}
+          </Await>
+        </Suspense>
       </div>
     </div>
   );
