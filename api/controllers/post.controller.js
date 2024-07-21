d9f458cecf7cdbdaa7909932d0c7ee15189108ce
Vidhanvyrs <vidhanvyrs@gmail.com>
2024-07-21 23:18:52 +0530

    fetching posts data based on url query

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,8 +1,22 @@
 import prisma from "../lib/prisma.js";
 
 export const getPosts = async (req, res) => {
+    const query = req.query;
+    // console.log(query);
+    //getting the query request and using it to find the posts from the db
   try {
-    const posts = await prisma.post.findMany();
+    const posts = await prisma.post.findMany({
+        where:{
+            city: query.city || undefined,
+            type: query.tpe || undefined,
+            property: query.property ||  undefined,
+            bedroom: parseInt(query.bedroom) || undefined,
+            price: {
+                gte: parseInt(query.minPrice) || 0,
+                lte: parseInt(query.maxPrice) || 10000000,
+            }
+        }
+    });
     res.status(200).json(posts);
   } catch (err) {
     res.status(500).json({ message: "Failed to get posts" });
--- client/src/lib/loaders.js
+++ client/src/lib/loaders.js
@@ -4,3 +4,13 @@ export const singlePageLoader = async ({ request, params }) => {
   const res = await apiRequest("/posts/" + params.id);
   return res.data;
 };
+
+export const listPageLoader = async ({ request, params }) => {
+  // console.log(request)
+  //getting the url using ?
+  const query = request.url.split("?")[1];
+  const res = await apiRequest("/posts?" + query);
+  return res.data;
+  // const res = await apiRequest("/posts/" + params.id);
+  // return res.data;
+};
